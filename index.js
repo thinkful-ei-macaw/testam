f88b0b254b4bd5a056dfa8db06c9ccefcf26dc9d
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-02-10 14:33:50 -0800

    Cracking the code decoded

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// 1
 function jediName(firstName, lastName) {
   return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
 
 console.log(jediName('Beyonce', 'Muhiddin'));
 
+// 2
 function beyond(num){
   if (num === Infinity){
     console.log('And beyond!');
@@ -21,3 +23,25 @@ function beyond(num){
 
 beyond(0);
 
+// 3 Cracking the code
+
+function decode(encoded) {
+  let words = encoded.split(' ');
+  let result = '';
+  for(let i = 0; i < words.length; i++) {
+    if(words[i][0] === 'a') {
+      result += words[i][1];
+    } else if(words[i][0] === 'b') {
+      result += words[i][2];
+    } else if(words[i][0] === 'c') {
+      result += words[i][3];
+    }else if(words[i][0] === 'd') {
+      result += words[i][4];
+    } else {
+      result += ' ';
+    }
+  }
+  return result;
+}
+
+console.log(decode('craft block argon meter bells brown croon droop'));
