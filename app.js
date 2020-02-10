4d301b0e29276fd706299d6558eced36a3d2240e
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-02-10 13:09:23 -0800

    added some minor changes

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ let myTeam = 'Agatha Muhiddin';
 console.log(myTeam);
 
 function createGreeting(name, age){
-    const yearOfBirth = 2020 - age;
-  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}.`;
+    const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
 }
 
 const greeting1 = createGreeting();
 console.log(greeting1);
+
+function getYearOfBirth(age) {
+  return 2020 - age;
+}
