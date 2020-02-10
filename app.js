519534087bc3ae2619d3a510415432f4b641574c
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-02-10 13:32:57 -0800

    Error handling update

--- app.js
+++ app.js
@@ -9,15 +9,20 @@ function createGreeting(name, age){
   return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Agatha', 20);
 console.log(greeting1);
 
 function getYearOfBirth(age) {
     if (age < 0){
         throw new Error("Age can not be negative");
-  return 2020 - age;
+    } else {
+      return 2020 - age;
+    }
+  }
+
+  try {
+    const greeting2 = createGreeting('Muhiddin', -20);
+} catch(e) {
+  console.log(e.message + "!!!!");
 }
-    try {
-        const greeting1 = createGreeting();
-    } catch(e)
-}
+    
