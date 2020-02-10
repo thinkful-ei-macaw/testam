520de9ae1c6c89621427f27af85d4792a68fbdae
Agatha North <rouborn@gmail.com>
2020-02-10 14:54:32 -0800

    months

--- index.js
+++ index.js
@@ -45,3 +45,43 @@ function decode(encoded) {
 }
 
 console.log(decode('craft block argon meter bells brown croon droop'));
+
+//4
+
+function howMany(month, year){
+  let result = '';
+
+  if ((year % 4 == 0 && year % 100 != 0 || year % 400 == 0)){
+    result += 'February has 29 days';
+  } else {
+  switch(month){
+    case 'January': result += 'January has 31 days';
+      break;
+    case 'February': result += 'February has 28 days';
+      break;
+    case 'March': result += 'March has 31 days';
+      break;
+    case 'April': result += 'April has 30 days';
+      break; 
+    case 'May': result += 'May has 31 days';
+      break;
+    case 'June': result += 'June has 30 days';
+      break;
+    case 'July': result += 'July has 31 days';
+      break;
+    case 'August': result += 'August has 31 days';
+      break;
+    case 'September': result += 'September has 30 days';
+      break; 
+    case 'October': result += 'October has 31 days';
+      break;
+    case 'November': result += 'November has 30 days';
+      break;
+    case 'December': result += 'December has 31 days';
+      break;
+    }
+  }
+  return result
+}
+
+console.log(howMany('February', 2020));
