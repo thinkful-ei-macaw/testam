10670a009f454f8e711a012817f34e9c6a917b5e
Agatha North <rouborn@gmail.com>
2020-02-10 13:16:30 -0800

    updates and errors

--- app.js
+++ app.js
@@ -13,5 +13,11 @@ const greeting1 = createGreeting();
 console.log(greeting1);
 
 function getYearOfBirth(age) {
+    if (age < 0){
+        throw new Error("Age can not be negative");
   return 2020 - age;
+}
+    try {
+        const greeting1 = createGreeting();
+    } catch(e)
 }
