ed9b6db39bc71829888f7944a6b09648dfb1a816
Agatha North <rouborn@gmail.com>
2020-02-10 14:17:52 -0800

    to infinity

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ function jediName(firstName, lastName) {
 }
 
 console.log(jediName('Beyonce', 'Muhiddin'));
+
+function beyond(num){
+  if (num === Infinity){
+    console.log('And beyond!');
+  }
+  else if (num !== Infinity && num > 0){
+    console.log('To Infinity!');
+  }
+  else if (num !== Infinity && num < 0){
+    console.log('To negative infinity!');
+  }
+  else if (num === 0){
+    console.log('Staying home');
+  }
+}
+
+beyond(0);
+
